9435755a9307c76077d9451a4526c349545975da
Naresh_Mahanta <naresh.mahanta@valtech.com>
2022-11-15 14:32:07 +0530

    added the useContext Hook

--- react-hooks/src/App.tsx
+++ react-hooks/src/App.tsx
@@ -5,6 +5,7 @@ import {Timer} from './hooks/useState';
 import {SelectingEl} from './hooks/useRef';
 import UseEffectComponent from './hooks/useEffect';
 import {UseReducerComponent} from './hooks/useReducer';
+import {UseContextComponent} from './hooks/useContext';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <Timer title =  'React useState Hook'/> */}
       {/* <SelectingEl title='React useRef and useEffect Hook' /> */}
       {/* <UseEffectComponent title='React useEffect and useLayoutEffect' /> */}
-      <UseReducerComponent title='React useReducer hook'/>
+      {/* <UseReducerComponent title='React useReducer hook'/> */}
+      <UseContextComponent title='React useContext hook' />
     </div>
   );
 }
---
+++ react-hooks/src/hooks/useContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useContext } from 'react';
+
+interface UseContextComponentProps {
+    title: string;
+}
+
+export const AppContext = createContext(null);
+export const UseContextComponent = ({title}:UseContextComponentProps) => {
+    return (
+        <>
+            <h2>{title}</h2>
+            <AppContext.Provider value={{title: 'Hello! there.'}}>
+                <LevelOneComponent />
+            </AppContext.Provider>
+        </>
+    );
+}
+
+
+const LevelOneComponent = () => {
+    return (
+        <div>
+            <LevelTwoComponent />
+        </div>
+    );
+}
+
+const LevelTwoComponent = () => {
+    const {title} = useContext(AppContext);
+    return (
+        <>
+            <p>{title}</p>
+        </>
+    );
+}
