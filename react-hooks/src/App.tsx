e783d81df54df686380c68696089becfa104f63b
Naresh_Mahanta <naresh.mahanta@valtech.com>
2023-07-18 13:42:14 +0530

    initial commit

--- react-hooks/src/App.tsx
+++ react-hooks/src/App.tsx
@@ -16,7 +16,7 @@ function App({title}:UseContextComponentProps) {
   const Description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis soluta facere, accusamus mollitia officiis sequi cupiditate nobis porro minus totam odit sunt, aspernatur dolores fugiat, modi nisi aliquam aperiam iste.';
   return (
     <div className="App">
-      <h2>ReactJS Hooks</h2>
+      <h2>ReactJS Hooks Deatails</h2>
       <p>
         <s>useState, useEffect,</s> useReducer, useContext, useRef, useCallback, useMemo
       </p>
